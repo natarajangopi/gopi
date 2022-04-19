e8001b63623a05fa40029fe276838d6ef3a12ef4
gopi1 <gopi.ceo@gmail.com>
2022-04-19 07:07:19 +0530

    demo1 java changes

---
+++ demo1.java
@@ -0,0 +1,9 @@
+public class demo1
+{
+ public static void main(String args[])
+ {
+  System.out.println("demo to add Java program in git");
+  System.out.println("2nd line");
+  System.out.println("3rd line");
+  }
+ } 
