6edf74f5e84f8b8845ba512f96bd5f853351e72c
gopi natarajan <41574725+natarajangopi@users.noreply.github.com>
2022-05-10 06:46:55 +0530

    Update demo1.java

--- demo1.java
+++ demo1.java
@@ -5,5 +5,6 @@ public class demo1
   System.out.println("demo to add Java program in git");
   System.out.println("2nd line");
   System.out.println("3rd line");
+   System.out.println("4th line");
   }
  } 
